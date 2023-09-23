d2858c7fa227daed8241f609e0b9024aa1d9c813
JeffcoatDesign <98057241+JeffcoatDesign@users.noreply.github.com>
2023-09-22 18:34:21 -0700

    Spell Mana Cost

--- CastForward/Assets/Scripts/PlayerSpells.cs
+++ CastForward/Assets/Scripts/PlayerSpells.cs
@@ -5,17 +5,37 @@ using UnityEngine.InputSystem;
 using SpellSystem;
 public class PlayerSpells : MonoBehaviour
 {
+    public delegate void UpdatePlayerMana(float playerMana, float maxMana);
+    public static event UpdatePlayerMana OnUpdatePlayerMana;
     public Spell leftSpell, rightSpell;
+    [SerializeField] private float _maxMana;
+    [SerializeField] private float _staminaRegenRate;
+    private float _currentMana;
     [SerializeField] private Transform playerLeftTransform;
     [SerializeField] private Transform playerRightTransform;
     public void OnLeftSpell (InputAction.CallbackContext ctx)
     {
-        if (ctx.performed && leftSpell != null)
+        bool canAffordSpell = _currentMana >= leftSpell.manaCost;
+        if (canAffordSpell && ctx.performed && leftSpell != null)
+        {
+            _currentMana -= leftSpell.manaCost;
             leftSpell.SummonSpell(playerLeftTransform.position, playerLeftTransform.rotation, true);
+            OnUpdatePlayerMana?.Invoke(_currentMana, _maxMana);
+        }
     }
     public void OnRightSpell(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed && rightSpell != null)
+        bool canAffordSpell = _currentMana >= rightSpell.manaCost;
+        if (canAffordSpell && ctx.performed && rightSpell != null)
+        {
+            _currentMana -= rightSpell.manaCost;
             rightSpell.SummonSpell(playerRightTransform.position, playerRightTransform.rotation, true);
+            OnUpdatePlayerMana?.Invoke(_currentMana, _maxMana);
+        }
+    }
+    private void FixedUpdate()
+    {
+        _currentMana = Mathf.Clamp(_currentMana + _staminaRegenRate, 0, _maxMana);
+        OnUpdatePlayerMana?.Invoke(_currentMana, _maxMana);
     }
 }
